2a070e1b43fc231b5af30a92c198a0e66e80d0b6
syedahalam <syedahalam@yahoo.com>
2021-10-17 07:21:41 -0400

    done

--- life.py
+++ life.py
@@ -1,11 +1,9 @@
-
-age = input("What is your current age?\n")
-
 #your age
+age = input("What is your current age?\n")
 a = int(age)
-# print(f"Your age in months is {a*12}\nin weeks is {a*52}\nand your age in days is {a*365}")
+print(f" - Your age in MONTHS is {a*12}\n - in WEEKS is {a*52}\n - and your age in DAYS is {a*365}\n")
 years_left = 90-a
 weeks_left = years_left*52
 months_left = years_left*12
 days_left = years_left*365
-print(f"Your age is {a}\n If you are alive till 90 years\n - You have {years_left} YEARS,\n - {weeks_left} WEEKS,\n - {months_left} MONTHS,\n - {days_left} DAYS\n   left to LIVE 🤩")
+print(f"Your age is {a}\n\n If you are alive till 90 years\n - You have {years_left} YEARS,\n - {weeks_left} WEEKS,\n - {months_left} MONTHS,\n - {days_left} DAYS\n   left to LIVE 🤩 Have a very Happy Life ahead! 🤩 ")
